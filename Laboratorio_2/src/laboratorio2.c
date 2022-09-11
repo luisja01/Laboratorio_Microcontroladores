715d568de8756c6ff3936b0e4d944a8911e9ed25
Luis <luisjaherrera@gmail.com>
2022-09-11 17:22:20 -0600

    avance de prueba

---
+++ Laboratorio_2/src/laboratorio2.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+int main(void)
+{
+
+    DDRB = 0x07; //Configuracion del puerto
+
+  //Parpadear
+  while (1) {
+    PORTB = 0x00; _delay_ms(5000);
+    PORTB = 0x01; _delay_ms(5000);
+    PORTB = 0x00; _delay_ms(5000);
+    PORTB = 0x02; _delay_ms(5000);
+    PORTB = 0x00; _delay_ms(5000);
+    PORTB = 0x04; _delay_ms(5000);
+  }
+
+    return 0;
+}
