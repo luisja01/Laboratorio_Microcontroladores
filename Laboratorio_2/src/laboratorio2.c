51076fc120c7d5c8ef0f14d926e81b6d6bf8e912
MonteJuanppy <montejuanppy@gmail.com>
2022-09-11 18:46:04 -0600

    Se intenta probar entrada de los botones

--- Laboratorio_2/src/laboratorio2.c
+++ Laboratorio_2/src/laboratorio2.c
@@ -6,15 +6,24 @@ int main(void)
 {
   //Se configuran los registros
   DDRB = 0b01111111; //Configuracion del puerto
+  DDRD = 0b11110011; //Entradas GPIOS 2 y 3
 
   //Parpadear
   while (1) {
+    /*
     PORTB = 0x00; _delay_ms(5000);
     PORTB = 0b01111111; _delay_ms(5000);
     PORTB = 0x00; _delay_ms(5000);
     PORTB = 0b01111111; _delay_ms(5000);
     PORTB = 0x00; _delay_ms(5000);
     PORTB = 0b01111111; _delay_ms(5000);
+    */
+   if ((PD2 == 1)|(PD3 == 1))
+   {
+    PORTB = 0b01111111; 
+   }
+   else PORTB = 0b00000000;
+   
   }
     return 0;
 }
