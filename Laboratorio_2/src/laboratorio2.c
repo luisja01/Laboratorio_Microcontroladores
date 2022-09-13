c44bfae1f52f6ab0e6d48dad9cad30a8a8cf9e88
Luis <luisjaherrera@gmail.com>
2022-09-12 20:16:26 -0600

    implementacion maquina de estados simple

--- Laboratorio_2/src/laboratorio2.c
+++ Laboratorio_2/src/laboratorio2.c
@@ -1,9 +1,28 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <stdbool.h>
+
+
+//Estados a utilizar
+#define peatonal 0
+#define vehicular 1 
+
+//Declaración de función
+void maquina_estados();
+
+//Variables a utilizar;
+int estado_actual;
+int estado_siguiente;
 
 int main(void)
 {
+
+  //Se inicializan los estados
+
+  estado_actual = peatonal;
+  estado_siguiente = peatonal;
+
   //Se configuran los registros
   DDRB = 0b01111111; //Configuracion del puerto
   DDRD = 0b11110011; //Entradas GPIOS 2 y 3
@@ -19,12 +38,36 @@ int main(void)
     PORTB = 0x00; _delay_ms(5000);
     PORTB = 0b01111111; _delay_ms(5000);
     */
-   if (PIND & ((1 << PD2)|(1 << PD3)))//Encender cuando se presiona un boton
-   {
-    PORTB = 0b01111111; 
-   }
-   else PORTB = 0b00000000;
+    estado_actual = estado_siguiente;
+    maquina_estados(); 
    
   }
     return 0;
+}
+
+
+//Función con implementación de máquina de estados
+void maquina_estados(){
+  
+  switch(estado_actual){
+
+    case peatonal:
+      PORTB = 0b01111000;
+
+      if (PIND & ((1 << PD2)|(1 << PD3)))//Si se estripa el botón cambiar de estado 
+      { 
+        estado_siguiente = vehicular; 
+      }
+      break;
+
+    case vehicular: 
+      PORTB = 0b00000111;
+         if (PIND & (~(1 << PD2)|~(1 << PD3)))// Si se cambia estado de botón cambiar de estado 
+        { 
+        estado_siguiente = peatonal; 
+        }
+        break; 
+
+  }
+
 }
