4c17dbef7efba9da2998f3fffddbd3723f791cf2
MonteJuanppy <montejuanppy@gmail.com>
2022-09-14 14:52:08 -0600

    Se agregan capacitores al circuito para que funcionen como filtros para eliminar el efecto rebote. Se modifican los estados del programa, asi como ordenarlo un poco. Hasta el momento se está utilizando una función de delay externa al micro y se realizan lecturas con polling, lo cual no es lo ideal pero permite ir manejando la estructura del programa

--- Laboratorio_2/src/laboratorio2.c
+++ Laboratorio_2/src/laboratorio2.c
@@ -1,46 +1,44 @@
+//Se incluten las librerias necesarias 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
 
-
 //Estados a utilizar
-#define peatonal 0
-#define vehicular 1 
+#define setup 0
+#define peatonal 1
+#define vehicular 2 
 
 //Declaración de función
 void maquina_estados();
 
 //Variables a utilizar;
+
 int estado_actual;
 int estado_siguiente;
 
+//Funcion principal
 int main(void)
 {
 
   //Se inicializan los estados
-
-  estado_actual = peatonal;
-  estado_siguiente = peatonal;
+  estado_siguiente = setup;
 
   //Se configuran los registros
   DDRB = 0b01111111; //Configuracion del puerto
   DDRD = 0b11110011; //Entradas GPIOS 2 y 3
-  PORTD = 0x00;
+  PIND = 0X00; //Se inicia en 0
+  PORTD = 0x00; //Se inicia en 0
+  PORTB = 0x00; //Se inicia en 0
+  sei(); //Se habilitan las interrupciones globales
 
-  //Parpadear
+  //Las entradas son PD2 y PD3, donde sus interrupciones son PCINT13 y PCINT14 respectivamente, o INT0 e INT1
+  //En este caso, las subrutinas de interrupciones cambiarán únicamente un cambio de estado. 
+  //Loop que se ejecuta siempre
   while (1) {
-    /*
-    PORTB = 0x00; _delay_ms(5000);
-    PORTB = 0b01111111; _delay_ms(5000);
-    PORTB = 0x00; _delay_ms(5000);
-    PORTB = 0b01111111; _delay_ms(5000);
-    PORTB = 0x00; _delay_ms(5000);
-    PORTB = 0b01111111; _delay_ms(5000);
-    */
-    estado_actual = estado_siguiente;
-    maquina_estados(); 
-   
+    estado_actual = estado_siguiente;//Se actualiza el estado_actual
+    maquina_estados(); //Se llama funcion de maquina de estados
+    //Esto de repite indefinidamente
   }
     return 0;
 }
@@ -49,24 +47,29 @@ int main(void)
 //Función con implementación de máquina de estados
 void maquina_estados(){
   
-  switch(estado_actual){
+  switch(estado_actual){//Se utiliza un switch para considerar cada caso
 
-    case peatonal:
-      PORTB = 0b01111000;
-
-      if (PIND & ((1 << PD2)|(1 << PD3)))//Si se estripa el botón cambiar de estado 
-      { 
-        estado_siguiente = vehicular; 
-      }
+    case setup:
+      PORTB = 0b00000000;
+      _delay_ms(10000);
+      estado_siguiente = vehicular; 
       break;
 
+    //Estado peatonal
+    case peatonal:
+      PORTB = 0b01111000;
+      _delay_ms(10000);
+      estado_siguiente = vehicular; 
+      break;//Se sale de la funcion
+
+    //Estado paso vehiculo
     case vehicular: 
       PORTB = 0b00000111;
-         if (PIND & (~(1 << PD2)|~(1 << PD3)))// Si se cambia estado de botón cambiar de estado 
-        { 
+      if (PIND & ((1 << PD2)|(1 << PD3)))//Si se estripa el botón cambiar de estado 
+      { 
         estado_siguiente = peatonal; 
-        }
-        break; 
+      }
+      break; //Se sale de la funcion
 
   }
 
