92480b10d06f619678a66bdd1a0b2e851b7fac3b
licit <licit@debian>
2022-10-26 09:33:21 -0600

    Cambios en repositorio

---
+++ Laboratorio_4/README.md
@@ -0,0 +1,2 @@
+# Laboratorio 4
+Directorio donde se trabaja lo que corresponde al laboratorio 4
---
+++ labo4/sdram.d
@@ -0,0 +1,21 @@
+sdram.o: sdram.c /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/pwr.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/fsmc.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/f4/fmc.h \
+ /home/juan/Documents/VIII_Semestre/Labo_Micros/Labos_con_Luisja/Laboratorio_Microcontroladores/Laboratorio_4/src/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/fmc_common_f47.h \
+ clock.h sdram.h
